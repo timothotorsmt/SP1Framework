09be6e0befa4e391d82954585677c19655dd4dc9
Timothy Luk <81371980+timothotorsmt@users.noreply.github.com>
2021-08-18 16:26:57 +0800

    Update Grid.h

--- SP1Framework/Grid.h
+++ SP1Framework/Grid.h
@@ -1,12 +1,14 @@
 #pragma once
 #include <string>
 #include <iostream>
+#include <fstream>
 #include <bitset>
 #include "Tile.h"
 class Grid
 {
 private:
 	Tile gridMap[15][35];
+	//set which doors are open and which are closed
 	std::bitset<4> doorConfig;
 	std::string name;
 	std::string room_pos;
