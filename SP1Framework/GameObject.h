98326a7b3d757727f834f870409a2df26f346a13
CHIA-QI-MING-JEREMY <tas7700123@gmail.com>
2021-08-20 13:56:28 +0800

    Update GameObject.h

--- SP1Framework/GameObject.h
+++ SP1Framework/GameObject.h
@@ -1,13 +1,30 @@
 #pragma once
 #include "Position.h"
+#include "Grid.h"
 
-class GameObject : public Position
+//made by dennis wong
+//edited by timothy
+//edited by jeremy
+class GameObject
 {
+protected:
+	Position objectPosition;
+	char entity_char;
+
 public:
+	//constructors and destructors
+	GameObject();
 	virtual ~GameObject();
 
+	//others
 	virtual void Interact(GameObject* obj) = 0;
 	virtual bool isCollided(GameObject* obj) = 0;
-	virtual bool checkForCollision(GameObject* obj, int direction) = 0;
+	virtual bool checkForCollision(Grid map, int direction) = 0;
 	virtual void MoveObject(int x, int y) = 0;
+
+	//getters and setters
+	//composition
+	Position getObjectPosition();
+	char getEntityChar();
+	void setEntityChar(char c);
 };
