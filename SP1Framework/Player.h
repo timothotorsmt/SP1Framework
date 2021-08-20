48beae0cfe51ede4c3c762bc5ba85f86f012a6a1
CHIA-QI-MING-JEREMY <tas7700123@gmail.com>
2021-08-20 13:48:49 +0800

    Update Player.h

--- SP1Framework/Player.h
+++ SP1Framework/Player.h
@@ -1,12 +1,17 @@
 #pragma once
 #include "GameObject.h"
+#include "Grid.h"
 
+//created by dennis wong
+//edited by timothy luk
 class Player : public GameObject
 {
 private:
 	static bool isPlayerCreate;
 	static int PocketMoney;
 	static bool JewelCapture;
+	int roomPosX;
+	int roomPosY;
 
 public:
 	Player();
@@ -14,11 +19,13 @@ public:
 
 	void Interact(GameObject* obj); // Interact with object that is overlapped and ready to be execute
 	bool isCollided(GameObject* obj); // Check if object is overlap with anything
-	bool checkForCollision(GameObject* obj, int direction); // Check if object possible to move
+	bool checkForCollision(Grid map, int direction); // Check if object possible to move
 	void MoveObject(int x, int y); // Move player and update position
+	int checkIsOOB();
+	void shiftRoomPos(int i, char c);
+	int getRoomPos(char c);
 
 	static bool CheckOnPlayer();
 	static int CheckOnMoney();
 	static bool CheckOnJewel();
 };
-
