d551c2ad69868660a0a427b719b4cf3e1f8c74a1
CHIA-QI-MING-JEREMY <tas7700123@gmail.com>
2021-08-20 13:37:26 +0800

    changed player to object
    standardised usage to objectPosition() and getObjectPosition() from playerPosition() and getPlayerPosition()

--- SP1Framework/Player.cpp
+++ SP1Framework/Player.cpp
@@ -1,80 +1,149 @@
 #include "Player.h"
-#include <iostream>
+
+//done by dennis wong
+//edited by timothy
 
 bool Player::isPlayerCreate = false;
 int Player::PocketMoney = 0;
 bool Player::JewelCapture = false;
 
+//done by dennis wong
+//edited by jeremy
 Player::Player()
 {
 	isPlayerCreate = true;
-	myPosition.SetMarker('P');
-	myPosition.UpdatePosition(10, 10, true);
+	entity_char = 'P';
+	objectPosition.setCoordinates(17, 7, true);
+	roomPosX = 3;
+	roomPosY = 3;
 
-	std::cout << "Player have been created!" << std::endl;
 }
 
+//done by dennis wong
 Player::~Player()
 {
 	isPlayerCreate = false;
-	std::cout << "Player have been destroyed!" << std::endl;
 }
 
+//done by dennis wong
 void Player::Interact(GameObject* obj)
 {
-	switch (obj->GetMarker())
+	switch (obj->getEntityChar())
 	{
-		case 'M':
-			PocketMoney++; // Default amount by 1
-			break;
+	case 'M':
+		PocketMoney++; // Default amount by 1
+		break;
 
-		case 'J':
-			JewelCapture = true; // Player got the jewel
-			break;
+	case 'J':
+		JewelCapture = true; // Player got the jewel
+		break;
 	}
 }
 
+//done by dennis wong
+//edited by jeremy
 bool Player::isCollided(GameObject* obj)
 {
-	if (myPosition.GetPosX() == obj->GetPosX() && myPosition.GetPosY() == obj->GetPosY())
+	if (objectPosition.isEqualPos(obj->getObjectPosition()))
 		return true;
-	else 
+	else
 		return false;
 }
 
-bool Player::checkForCollision(GameObject* obj, int direction) 
+//done by dennis wong
+//edited by timothy luk
+//edited by jeremy
+bool Player::checkForCollision(Grid map, int direction)
 {
 	switch (direction)
 	{
-		case 1: // UP
-			if (myPosition.GetPosX() == obj->GetPosX() && myPosition.GetPosY() - 1 == obj->GetPosY())
+	case 1: // UP
+		if (objectPosition.getRow() != 0) {
+			if (map.getTile(objectPosition.getColumn(), objectPosition.getRow() - 1).get_tile_char() == '#') {
 				return true;
-
-		case 2: // LEFT
-			if (myPosition.GetPosX() - 1 == obj->GetPosX() && myPosition.GetPosY() == obj->GetPosY())
+			}
+		}
+		break;
+	case 2: // LEFT
+		if (objectPosition.getColumn() != 0) {
+			if (map.getTile(objectPosition.getColumn() - 1, objectPosition.getRow()).get_tile_char() == '#') {
 				return true;
-
-		case 3: // DOWN
-			if (myPosition.GetPosX() == obj->GetPosX() && myPosition.GetPosY() + 1 == obj->GetPosY())
+			}
+		}
+		break;
+	case 3: // DOWN
+		if (objectPosition.getRow() != 14) {
+			if (map.getTile(objectPosition.getColumn(), objectPosition.getRow() + 1).get_tile_char() == '#') {
 				return true;
-
-		case 4: // RIGHT
-			if (myPosition.GetPosX() + 1 == obj->GetPosX() && myPosition.GetPosY() == obj->GetPosY())
+			}
+		}
+		break;
+	case 4: // RIGHT
+		if (objectPosition.getColumn() != 34) {
+			if (map.getTile(objectPosition.getColumn() + 1, objectPosition.getRow()).get_tile_char() == '#') {
 				return true;
-
-		default: // STOP
-			break;
+			}
+		}
+		break;
+	default: // STOP
+		break;
 	}
-
-	// If there is no collision
+	//if no collision return true
 	return false;
 }
 
+//done by dennis wong
+//edited by jeremy
 void Player::MoveObject(int x, int y)
 {
-	myPosition.UpdatePosition(x, y, false);
+	objectPosition.setCoordinates(x, y, false);
 }
 
+//done by timothy
+//edited by jeremy
+int Player::checkIsOOB()
+{
+	if (objectPosition.getColumn() == 0) {
+		objectPosition.setCoordinates(33, 0, false);
+		shiftRoomPos((-1), 'x');
+		return 2;
+	}
+	else if (objectPosition.getColumn() == 34) {
+		objectPosition.setCoordinates(-33, 0, false);
+		shiftRoomPos((1), 'x');
+		return 4;
+	}
+	else if (objectPosition.getRow() == 0) {
+		objectPosition.setCoordinates(0, 13, false);
+		shiftRoomPos((-1), 'y');
+		return 1;
+	}
+	else if (objectPosition.getRow() == 14) {
+		objectPosition.setCoordinates(0, -13, false);
+		shiftRoomPos((1), 'y');
+		return 3;
+	}
+	else return 0;
+}
+
+//done by timothy
+void Player::shiftRoomPos(int i, char c)
+{
+	if (c == 'x') {
+		roomPosX += i;
+	}
+	else {
+		roomPosY += i;
+	}
+}
+
+//done by timothy
+int Player::getRoomPos(char c)
+{
+	return (c == 'x') ? (roomPosX) : (roomPosY);
+}
+
+//done by dennis
 bool Player::CheckOnPlayer()
 {
 	return isPlayerCreate;
