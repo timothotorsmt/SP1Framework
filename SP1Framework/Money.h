8475407530813e984669d6134b72c8bc0fc625a7
CHIA-QI-MING-JEREMY <tas7700123@gmail.com>
2021-08-20 13:58:31 +0800

    Update Money.h

--- SP1Framework/Money.h
+++ SP1Framework/Money.h
@@ -1,20 +1,21 @@
 #pragma once
 #include "GameObject.h"
+#include "Grid.h"
+
+//done by dennis wong
 class Money : public GameObject
 {
 private:
 	static int totalMoney;
 
 public:
-	Money();
-	Money(int x, int y);
+	Money(Grid level_map);
 	virtual ~Money();
 
 	void Interact(GameObject* obj); // Interact with object that is overlapped and ready to be execute
 	bool isCollided(GameObject* obj); // Check if object is overlap with anything
-	bool checkForCollision(GameObject* obj, int direction); // Check if object possible to move
-	void MoveObject(int x, int y); // If money have leg to walk on his own?
+	bool checkForCollision(Grid map, int direction); // Check if object possible to move
+	void MoveObject(int x, int y) override; // If money have leg to walk on his own?
 
 	static int GetMoneyCount();
 };
-
