68406b35c28c52de1acd10b1fa6925bf91d87db4
Timothy Luk <81371980+timothotorsmt@users.noreply.github.com>
2021-08-24 14:22:19 +0800

    Create leaderboard.h

---
+++ SP1Framework/leaderboard.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <queue>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "Framework/console.h"
+#define BACKMATCHTEXT (BACKGROUND_INTENSITY | FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED)
+
+class Leaderboard
+{
+private:
+	std::vector<std::pair<int, std::string>> leaderboard;
+public:
+	Leaderboard();
+	~Leaderboard();
+
+	//getters
+	bool find_name_exists(std::string name);
+	
+	//setters
+	void add_new_score(std::string name, int score);
+	void resetLeaderboard();
+	void importLeaderboard();
+	void exportLeaderboard();
+
+	//others 
+	void printLeaderboard(Console& g_Console, unsigned int i);
+};
