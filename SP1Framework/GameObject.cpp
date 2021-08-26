de03bfac31f767e7fc0446c00b038a1d0a4ecdde
CHIA-QI-MING-JEREMY <tas7700123@gmail.com>
2021-08-27 04:56:13 +0800

    Update GameObject.cpp

--- SP1Framework/GameObject.cpp
+++ SP1Framework/GameObject.cpp
@@ -1,7 +1,6 @@
 #include "GameObject.h"
 #include <iostream>
 
-//done by dennis wong
 GameObject::GameObject()
 {
 	entity_char = ' ';
@@ -11,20 +10,16 @@ GameObject::~GameObject()
 {
 }
 
-//done by timothy
-//edited by jeremy
 Position GameObject::getObjectPosition()
 {
 	return objectPosition;
 }
 
-//done by dennis wong
 char GameObject::getEntityChar()
 {
 	return entity_char;
 }
 
-//done by dennis wong
 void GameObject::setEntityChar(char c)
 {
 	entity_char = c;
