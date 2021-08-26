4830e6809d32e0084c94428cfc38c3019b45c884
CHIA-QI-MING-JEREMY <tas7700123@gmail.com>
2021-08-27 04:55:53 +0800

    Update Enemy.h

--- SP1Framework/Enemy.h
+++ SP1Framework/Enemy.h
@@ -2,11 +2,10 @@
 #include "GameObject.h"
 #include "Grid.h"
 
-//created by jeremy chia
 class Enemy : public GameObject
 {
 private:
-	static bool isEnemyCreate;
+	static int numOfEnemies;
 	int enemyPosX{};
 	int enemyPosY{};
 	int roomPosX{};
@@ -17,12 +16,14 @@ public:
 	Enemy(int enemyPosX, int enemyPosY, int roomPosX, int roomPosY);
 	virtual ~Enemy();
 
-	void Interact(GameObject* obj);
 	bool isCollided(GameObject* obj);
+	void Interact(GameObject* obj);
 	bool checkForCollision(Grid map, int direction);
 	void MoveObject(int x, int y);
 
 	int getEnemyPosX(void);
 	int getEnemyPosY(void);
-};
 
+	int getRoomPosX(void);
+	int getRoomPosY(void);
+};
