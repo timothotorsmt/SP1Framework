b01f79c4b1b763b2c70e563cd6ea56d67357015c
CHIA-QI-MING-JEREMY <tas7700123@gmail.com>
2021-08-27 04:57:43 +0800

    Update Map.h

--- SP1Framework/Map.h
+++ SP1Framework/Map.h
@@ -1,14 +1,20 @@
 #pragma once
-#include <bits/stdc++.h>
+#include <array>
+#include <iostream>
+#include <vector>
+#include <random>
 #include "Grid.h"
+#include "Player.h"
 #include "Tile.h"
+
 class Map
 {
 private:
 	char minmap[4][4];
 	Grid level_map[4][4];
 	bool is_stolen;
-	int isCurrentlyIn[2];
+
+	std::vector<std::string> filenames{ "Haute Couture Gallery.txt", "The Garden.txt", "The Prehistoric Exhibit.txt"};
 public:
 	Map();
 	~Map();
@@ -16,11 +22,13 @@ public:
 	//getters
 	char get_minmap_char(int x, int y);
 	Grid get_map_grid(int x, int y);
-	bool check_is_stolen();
-	int Map::getCurrentBox(int i);
+	bool check_is_stolen(Player& player);
 	std::string getObjective();
 
 	//setters
-	void update_minmap_char();
-	void moveBox(int direction);
+	void update_minmap_char(Player& player);
+
+	//others
+	void distributerandomfiles();
+	void generateNewMap();
 };
