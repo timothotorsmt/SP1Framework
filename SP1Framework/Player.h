114746329ba4f4fc219c07032aa076579004e827
CHIA-QI-MING-JEREMY <tas7700123@gmail.com>
2021-08-27 05:03:20 +0800

    Update Player.h

--- SP1Framework/Player.h
+++ SP1Framework/Player.h
@@ -2,14 +2,12 @@
 #include "GameObject.h"
 #include "Grid.h"
 
-//created by dennis wong
-//edited by timothy luk
 class Player : public GameObject
 {
 private:
 	static bool isPlayerCreate;
 	static int PocketMoney;
-	static bool JewelCapture;
+	static bool jewelCaptureStatus;
 	int roomPosX;
 	int roomPosY;
 
@@ -17,15 +15,19 @@ public:
 	Player();
 	virtual ~Player();
 
-	void Interact(GameObject* obj); // Interact with object that is overlapped and ready to be execute
-	bool isCollided(GameObject* obj); // Check if object is overlap with anything
-	bool checkForCollision(Grid map, int direction); // Check if object possible to move
-	void MoveObject(int x, int y); // Move player and update position
+	void Interact(GameObject* obj);				// Interact with object that is overlapped and ready to be execute
+	bool isCollided(GameObject* obj);			// Check if object is overlap with anything
+	bool checkForCollision(Grid map, int direction);	// Check if object possible to move
+	void MoveObject(int x, int y);				// Move player and update position
 	int checkIsOOB();
 	void shiftRoomPos(int i, char c);
 	int getRoomPos(char c);
 
-	static bool CheckOnPlayer();
+	static void setPlayerAliveStatus(bool playerAliveStatus);
+	static bool isPlayerAlive();
+
 	static int CheckOnMoney();
-	static bool CheckOnJewel();
+
+	static void setJewelCaptureStatus(bool jewelStatus);
+	static bool isJewelCaptured();
 };
